f5e36ff8814f87cfe8bea681af6e0e218fcd1afb
Davy <davy@datalink.be>
2025-11-17 13:51:34 +0100

    fix: catch some more pagination null exceptions

--- resources/ts/types/pagination.d.ts
+++ resources/ts/types/pagination.d.ts
@@ -9,15 +9,15 @@ export interface PaginationData<T = any> {
     current_page: number;
     data: T[];
     first_page_url: string;
-    from: number;
+    from?: number | null;
     last_page: number;
     links: Array<LinkProps>;
     next_page_url?: string | null;
     path: string;
     prev_page?: number | null;
     prev_page_url?: string | null;
-    to: number;
-    total: number;
+    to?: number | null;
+    total?: number | null;
 }
 
 // type without the data array for pagination component
