ae87b211146935156c19c32f19802af2316ec1c1
Davy <davy@datalink.be>
2025-11-19 11:16:00 +0100

    refactor: implement pinia to app.ts, add missing typing

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -2,19 +2,24 @@ import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import type { DefineComponent } from "vue";
 import { createApp, h } from "vue";
+import { createPinia } from 'pinia';
 import { ZiggyVue } from "ziggy-js";
+
+
+const pinia = createPinia();
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 createInertiaApp({
-    title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) =>
+    title: (title: any) => (title ? `${title} - ${appName}` : appName),
+    resolve: (name: any) =>
         resolvePageComponent(
             `./pages/${name}.vue`,
             import.meta.glob<DefineComponent>("./pages/**/*.vue"),
         ),
-    setup({ el, App, props, plugin }) {
+    setup({ el, App, props, plugin }: any) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(pinia)
             .use(ZiggyVue)
             .mount(el);
     },
